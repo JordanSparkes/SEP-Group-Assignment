e7e8756b89470e600943675ad919e80fa3a8944b
I2hys <rhys.richards@hotmail.co.uk>
2015-04-05 20:19:37 +0100

    Added starting point
    Header and main set up

---
+++ SEP Group Game/SEP Group Game/lootData.h
@@ -0,0 +1,12 @@
+#define BITMAP_ID 0x4D42		// the universal bitmap ID
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int lootID;
+int lootDamage;
+int lootDefence;
+
+void fileIO();
---
+++ SEP Group Game/SEP Group Game/lootInput.cpp
@@ -0,0 +1,6 @@
+#include "lootData.h"
+
+void fileIO()
+{
+
+}
