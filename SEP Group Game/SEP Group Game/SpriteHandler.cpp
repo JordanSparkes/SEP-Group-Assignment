9f899310723b668ce75d545b1b1ced5e14c7efad
Gary-Keen <gkeen.138646@student.uwtsd.ac.uk>
2015-04-08 21:21:31 +0100

    Added a cpp file for Sprite Handler, Broken due to placeholder values. Partial collab. Mikey.

--- SEP Group Game/SEP Group Game/SpriteHandler.cpp
+++ SEP Group Game/SEP Group Game/SpriteHandler.cpp
@@ -2,10 +2,139 @@
 
 SpriteHandler::SpriteHandler(void)
 {
+	speed = 0; //Set the speed to 0 for the constructor.
 
+	spriteSheet = NULL; //set the pointers to null for the constructor.
 }
 
 SpriteHandler::~SpriteHandler(void)
 {
+	SDL_FreeSurface(spriteSheet); //use SDL to remove the SDL_Surface from memory upon deconstruction.
 
+	spriteSheet = NULL; //Set pointers to null after use.
+}
+
+SDL_Surface* SpriteHandler::loadImage(char* dir)
+{
+	SDL_Surface* original; //Pointer to original, raw image. Should this be Predefined for constructor/destructor?
+	SDL_Surface* optimized; //Pointer to SDl optimized image. Should this be Predefined for constructor/destructor?
+
+	original = SDL_LoadBMP(dir); //Images have to be BMP for SDL as it is the raw, uncompressed format.
+
+	if(original !=NULL) //If the image is not null (most likely case comes first).
+	{
+		optimized = SDL_DisplayFormat(original); //Optimize the image for SDL to use)
+		
+		SDL_FreeSurface(original); //Remove the original unoptimized image from memory.
+	}
+	else //If the image is null.
+	{
+		printf("Error: The sprite you are trying to load does not exist."); //Error message as image has not been allocated. this is purely here for testing.
+		exit(1);
+	}
+	return optimized; //Return the optimized version of the image.
+}
+
+SDL_Surface* SpriteHandler::loadImage(char* dir, Uint8 r, Uint8 g, Uint8 b)
+{
+	SDL_Surface* original;
+	SDL_Surface* optimized;
+
+	original = SDL_LoadBMP(dir);
+
+	if(original != NULL)
+	{
+		optimized = SDL_DisplayFormat(original);
+
+		Uint32 ignoreColour = SDL_MapRGB(optimized->format,r,g,b); //Create a unsigned 32bit int to define the colour of the bitmap to ignore.
+
+		SDL_SetColorKey(optimized,SDL_SRCCOLORKEY,ignoreColour); //SDL sets any pixel that has the RGB value of the colour key to transparent.
+		SDL_FreeSurface(original);
+	}
+	else
+	{
+		printf("Error: The sprite you are trying to load does not exist."); //Error message as image has not been allocated. this is purely here for testing.
+		exit(1);
+	}
+	return optimized;
+}
+
+void SpriteHandler::setSpeed(int value)
+{
+	speed = value; //Sets the speed to the value that has been passed in.
+}
+
+int SpriteHandler::getSpeed()
+{
+	return speed; //Gets the speed value.
+}
+
+void SpriteHandler::setX(int X)
+{
+	rect.x = X;
+}
+
+void SpriteHandler::setY(int Y)
+{
+	rect.y = Y;
+}
+
+int SpriteHandler::getX()
+{
+	return rect.x;
+}
+
+int SpriteHandler::getY()
+{
+	return rect.y;
+}
+
+void SpriteHandler::setWidth(int W)
+{
+	rect.w = W;
+}
+
+void SpriteHandler::setHeight(int H)
+{
+	rect.h = H;
+}
+
+int SpriteHandler::getWidth()
+{
+	return rect.w;
+}
+
+int SpriteHandler::getHeight()
+{
+	return rect.h;
+}
+
+float SpriteHandler::getCenter() //The contents of this function are very temporary.
+{
+	return rect.x+(rect.w/2.0f);
+	return rect.y+(rect.h/2.0f);
+}
+
+SDL_Rect* SpriteHandler::loadSprites(const int quant, const int offset, const int width, const int height)
+{
+	SDL_Rect* animations = new SDL_Rect[quant]; //Create an array containing the number of animations the sprite sheet contains.
+
+	for(int i = 0; i < quant; i++) //Simple loop through the array.
+	{
+		animations[i].x = offset*width; //For the current animation set, the X value = the offset times the width.
+		animations[i].y = i*height; //For the current animation set, the Y value = the current array number times the height.
+		animations[i].w = width; //For the current animation set, the width = the predefined width value.
+		animations[i].h = height; //For the current animation set, the height = the predefined height value.
+	}
+	return animations;
+}
+
+bool SpriteHandler::windowBorders()
+{
+	if(rect.y > 0 && (rect.y + rect.h) < game::Height && rect.x > 0 && (rect.x + rect.w) < game::Width) //If the sprite is within all 4 borders, do nothing. This if statement comes first to save on processing power.
+		return false;
+	else //Else, check if corners are being breached. If so, "move the screen" to reveal more of the map and stuff.
+	{
+		//Do this later. As we are checking all 4 sides in one function (may be unoptimized, do some testing later), turn the if statements into a case statement to save space and neaten code.
+	}
 }
--- SEP Group Game/SEP Group Game/SpriteHandler.h
+++ SEP Group Game/SEP Group Game/SpriteHandler.h
@@ -16,26 +16,26 @@ public:
 	SpriteHandler(void); //Constructor
 	virtual ~SpriteHandler(void); //Virtual destructor. virtual because other classes will use it.
 
-	static SDL_Surface* loadImage(char* dir); //Static function to load an image from file.
-	static SDL_Surface* loadImage(char* dir, Uint8 r, Uint8 g, Uint8 b); //Overloaded static funtion to load an image from file, using a defined RGB value as an ignore colour.
+	static SDL_Surface* loadImage(char* dir); //Static function to load and optimize an image from file.
+	static SDL_Surface* loadImage(char* dir, Uint8 r, Uint8 g, Uint8 b); //Overloaded static funtion to load and optimize an image from file, using a defined RGB value as an ignore colour.
 
-	int setSpeed(int); //Function to set the speed value.
+	void setSpeed(int); //Function to set the speed value.
 	int getSpeed(); //Function to retreive the speed value.
 
-	bool windowBorders(); //Function to check if a sprite is going out of the screen. This function is temporary as the map will extend outside the window.
-
-	int setX(); //Function to change X position of sprite.
-	int setY(); //Function to change Y position of sprite.
-	int getX(int); //Function to retreive X position of sprite.
-	int getY(int); //Function to retreive Y position of sprite.
+	void setX(int); //Function to change X position of sprite.
+	void setY(int); //Function to change Y position of sprite.
+	int getX(); //Function to retreive X position of sprite.
+	int getY(); //Function to retreive Y position of sprite.
 	//It was considered that these 2 sets of functions be merged but as the sprites will always be walking either vertically or horizontally it seems quicker to process if they are seperate.
+
+	void setWidth(int); //Function to set the sprite frames width.
+	void setHeight(int); //Function to set the sprite frames height.
+	int getWidth();  //Function to retreive the sprite frames width.
+	int getHeight(); //Function to retreive the sprite frames height.
 
 	float getCenter(); //Function to define the center of each sprite frame. This will be a single function unless the height and width values are different.
 
-	int setWidth(int); //Function to set the sprite frames width.
-	int setHeight(int); //Function to set the sprite frames height.
-	int getWidth();  //Function to retreive the sprite frames width.
-	int getHeight(); //Function to retreive the sprite frames height.
+	bool windowBorders(); //Function to check if a sprite is going out of the screen. This function is temporary as the map will extend outside the window.
 
 	virtual void init(SDL_Surface* spriteSheet, SDL_Rect* position) = 0; //Virtual function to initialize sprite stuff. To be used by other classes.
 	virtual void blit(SDL_Surface* window) = 0; //Virtual function to draw sprite to screen. To be used by other classes.
